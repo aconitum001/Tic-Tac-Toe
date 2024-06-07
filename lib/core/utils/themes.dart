9f78cab82b749ccc47920cb7cb34a5fd2c04b509
aconitum001 <skandern0000@gmail.com>
2024-06-07 20:58:40 +0100

    create darkTheme

---
+++ lib/core/utils/themes.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:tic_tac_toe/core/utils/colors.dart';
+
+ThemeData darkTheme = ThemeData(
+  brightness: Brightness.dark,
+  colorScheme: ColorScheme.dark(
+    surface: AppColors.primary1,
+    primary: AppColors.secondary2,
+    secondary: AppColors.secondary1[0],
+    onSecondary: AppColors.secondary1[1],
+    onSecondaryContainer: AppColors.grey3,
+    primaryContainer: AppColors.grey3,
+    onPrimaryContainer: AppColors.grey2,
+  ),
+  scaffoldBackgroundColor: AppColors.primary1,
+);
