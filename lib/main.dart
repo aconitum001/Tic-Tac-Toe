f64ec04802c9994952036c4db13f297325d12f6c
aconitum001 <skandern0000@gmail.com>
2024-06-07 20:43:58 +0100

    first comit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const TicTacToe());
+}
+
+class TicTacToe extends StatelessWidget {
+  const TicTacToe({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
