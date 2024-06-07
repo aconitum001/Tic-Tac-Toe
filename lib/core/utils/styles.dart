6aa4b9fc25dc8dfa7dc6528784a04c03babbe568
aconitum001 <skandern0000@gmail.com>
2024-06-07 21:10:38 +0100

    create app styles

---
+++ lib/core/utils/styles.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+abstract class Styles {
+  static TextStyle style45 = TextStyle(
+    fontSize: 45.sp,
+    fontWeight: FontWeight.w700,
+  );
+
+  static TextStyle style = TextStyle(
+    fontSize: 20.sp,
+    fontWeight: FontWeight.w700,
+  );
+
+  static TextStyle style17 = TextStyle(
+    fontSize: 17.sp,
+    fontWeight: FontWeight.w600,
+  );
+
+  static TextStyle style15 = TextStyle(
+    fontSize: 15.sp,
+    fontWeight: FontWeight.w500,
+  );
+
+  static TextStyle style13 = TextStyle(
+    fontSize: 13.sp,
+    fontWeight: FontWeight.w500,
+  );
+
+  static TextStyle style19 = TextStyle(
+    fontSize: 19.sp,
+    fontWeight: FontWeight.w500,
+  );
+
+  static TextStyle style37 = TextStyle(
+    fontSize: 37.sp,
+    fontWeight: FontWeight.w800,
+  );
+
+  static TextStyle style30 = TextStyle(
+    fontSize: 30.sp,
+    fontWeight: FontWeight.w800,
+  );
+
+  static TextStyle style25 = TextStyle(
+    fontSize: 25.sp,
+    fontWeight: FontWeight.w700,
+  );
+
+  static TextStyle style40 = TextStyle(
+    fontSize: 40.sp,
+    fontWeight: FontWeight.w700,
+  );
+
+  static TextStyle style35 = TextStyle(
+    fontSize: 35.sp,
+    fontWeight: FontWeight.w700,
+  );
+
+  static TextStyle style12 = TextStyle(
+    fontSize: 12.85.sp,
+    fontWeight: FontWeight.w400,
+  );
+
+  static TextStyle style14 = TextStyle(
+    fontSize: 14.sp,
+    fontWeight: FontWeight.w700,
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() {
   runApp(const TicTacToe());
@@ -9,8 +10,13 @@ class TicTacToe extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
+    return ScreenUtilInit(
+      designSize: const Size(414, 896),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) => MaterialApp.router(
+        debugShowCheckedModeBanner: false,
+      ),
     );
   }
 }
