6fcd0b5224db02242a65d579f1ce8ce5fc90d01a
aconitum001 <skandern0000@gmail.com>
2024-06-07 20:44:36 +0100

    create app colors

---
+++ lib/core/utils/colors.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+abstract class Colors {
+  static const Color primary1 = Color(0xff232C46);
+  static const Color primary2 = Color(0xff29324B);
+  static const Color primary3 = Color(0xff3F4E6B);
+
+  static const LinearGradient secondary1 = LinearGradient(
+    colors: [
+      Color(0xffA2B0D3),
+      Color(0xff889BCC),
+    ],
+  );
+  static const Color secondary2 = Color(0xff889BCC);
+  static const Color secondary3 = Color(0xffA2B0D3);
+
+  static const Color grey1 = Color(0xff000000);
+  static const Color grey2 = Color(0xff1E1E1E);
+  static const Color grey3 = Color(0xffFFFFFF);
+}
