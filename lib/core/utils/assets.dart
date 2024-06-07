291f06bd2bee6348fd9d887b98b28778cafc135a
aconitum001 <skandern0000@gmail.com>
2024-06-07 21:33:12 +0100

    create appAssets

---
+++ lib/core/utils/assets.dart
@@ -0,0 +1,27 @@
+abstract class Assets {
+  static const String logo = 'assets/images/logo.svg';
+
+  static const String primaryDarkBackground =
+      "assets/images/backgrounds/primary_dark_background.svg";
+
+  static const String gameDraw = "assets/images/game_draw.svg";
+  static const String gameWin = "assets/images/game_win.svg";
+
+  static const String rulesWin = "assets/images/rules_win.svg";
+  static const String rulesLose = "assets/images/rules_lose.svg";
+  static const String rulesDraw = "assets/images/rules_draw.svg";
+
+  static const String xStyle1 = "assets/images/x_style1.svg";
+  static const String oStyle1 = "assets/images/o_style1.svg";
+
+  static const String avtar1 = "assets/images/avatars/avatar1.svg";
+  static const String avtar2 = "assets/images/avatars/avatar2.svg";
+  static const String avtar3 = "assets/images/avatars/avatar3.svg";
+  static const String avtar4 = "assets/images/avatars/avatar4.svg";
+  static const String avtar5 = "assets/images/avatars/avatar5.svg";
+  static const String avtar6 = "assets/images/avatars/avatar6.svg";
+  static const String avtar7 = "assets/images/avatars/avatar7.svg";
+  static const String avtar8 = "assets/images/avatars/avatar8.svg";
+
+  static const String botAvatar1 = "assets/images/avatars/bot_avatar1.svg";
+}
